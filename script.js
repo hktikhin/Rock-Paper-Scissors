06b61bc43a4591c081921ee86fc150e89d23e31f
hktikhin <20tikhin@gmail.com>
2021-07-29 13:15:58 +0800

    make it interative

--- script.js
+++ script.js
@@ -1,62 +1,95 @@
 function game(){
-    let computerSelection;
-    let playerChoice;
-    let playerSelection;
     let playerScore=0;
     let computerScore=0;
+    let roundCounter = 0;
 
     function computerPlay(){
         let choice = Math.floor(3*Math.random()+1);
         return (choice === 1)?"Rock":(choice === 2)?"Paper": "Scissors" ; 
     }
 
-    function playRound(playerSelection, computerSelection) {
-        playerSelection = playerSelection[0].toUpperCase()+playerSelection.slice(1).toLowerCase()
+    function playRound(event) {
+        let computerSelection = computerPlay();
+        let playerSelection = event.target.textContent;
+        const body = document.querySelector("body");
+        // remove previous div node from dom tree if having 
+        const previousDivNode = document.querySelector(".round-result");
+        if (previousDivNode) previousDivNode.remove();
+        const resultDiv = document.createElement("div");
+        resultDiv.classList.add("round-result") ;
+        const resultRound = document.createElement("p");
+        const resultScore = document.createElement("p");
+         
         if (playerSelection === computerSelection){
-            return `Tie! ${playerSelection} equals ${computerSelection}`
+            resultRound.textContent = `Tie! ${playerSelection} equals ${computerSelection}`
         }
         switch(true){
             case playerSelection  === "Rock" && computerSelection === "Paper" :
                 computerScore++;
-                return "You Lose! Paper beats Rock";
+                resultRound.textContent = "You Lose! Paper beats Rock";
+                break;
             case playerSelection  ==="Rock" && computerSelection === "Scissors" :
                 playerScore++;
-                return "You Win! Rock beats Scissors";
+                resultRound.textContent = "You Win! Rock beats Scissors";
+                break;
             case playerSelection  ==="Paper" && computerSelection === "Rock"  :
                 playerScore++;
-                return "You Win! Paper beats Rock";
+                resultRound.textContent = "You Win! Paper beats Rock";
+                break;
             case playerSelection  ==="Paper" && computerSelection ===  "Scissors":
                 computerScore++;
-                return "You Lose! Scissor beats Paper";
+                resultRound.textContent = "You Lose! Scissor beats Paper";
+                break;
             case playerSelection  ==="Scissors" && computerSelection ===  "Rock" :
                 computerScore++;
-                return "You Lose! Rock beats Scissors";
+                resultRound.textContent = "You Lose! Rock beats Scissors";
+                break;
             case playerSelection  === "Scissors"&& computerSelection === "Paper" :
                 playerScore++;
-                return "You Win! Scissors beats Paper";
+                resultRound.textContent = "You Win! Scissors beats Paper";
+                break;
+        }
+
+        resultScore.textContent = `Round ${++roundCounter}.Player Score:${playerScore},Computer Score:${computerScore}.`;
+        body.appendChild(resultDiv);
+        resultDiv.appendChild(resultRound);
+        resultDiv.appendChild(resultScore);
+
+        if(playerScore === 5 || computerScore === 5){
+            const finalResult = document.createElement("p");
+            if(playerScore > computerScore){
+                finalResult.textContent = "Player Win!";
+            }
+            else{
+                finalResult.textContent = "Player Lose!";
+            }
+            // clear the buttons, add new button in below 
+            const buttonDiv = document.querySelector(".buttons");
+            body.removeChild(buttonDiv);
+            const  newButton =  document.createElement("button");
+            newButton.textContent = "Replay?"
+            newButton.addEventListener("click",() => replayGame(buttonDiv));
+            resultDiv.appendChild(finalResult);
+            resultDiv.appendChild(newButton);
         }
     }
 
-    for(let i =0; i<5;i++){
-        do{
-            playerChoice = prompt(`Round ${i+1}.What is your choice?(1:Rock,2:Paper,3:Scissors)`)
-            playerSelection = (playerChoice === "1")?"Rock":(playerChoice === "2")?"Paper": 
-            (playerChoice === "3")?"Scissors":undefined;
-        }while(playerSelection === undefined)
+    function replayGame(buttonDiv){
+        const body = document.querySelector("body");
+        // clear the result div 
+        const child = body.lastElementChild;
+        child.remove();
+        body.appendChild(buttonDiv);
+        // reset the score 
+        playerScore=0;
+        computerScore=0;
+        roundCounter = 0;
+    }
 
-        computerSelection =  computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Round ${i+1}.Player Score:${playerScore},Computer Score:${computerScore}.`);
-    }
-    if(playerScore === computerScore){
-        console.log("Tie!");
-    }
-    else if(playerScore > computerScore){
-        console.log("You Win!");
-    }
-    else{
-        console.log("You Lose!");
-    }
+    let buttons = Array.from(document.querySelectorAll(".button"));
+    buttons.forEach(button => {
+        button.addEventListener("click",playRound);
+    });
 }
 
 game();
